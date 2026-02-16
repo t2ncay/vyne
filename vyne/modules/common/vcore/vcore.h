16dd643af3ee9d2a9924001ca52f64c4e4af4b15
Tuncay <68643463+tuncaygafarli@users.noreply.github.com>
2026-02-16 21:01:40 +0400

    modules : moved all core modules to `common` folder

---
+++ vyne/modules/common/vcore/vcore.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <ctime>
+#include <chrono>
+#include <thread>
+#include <random>
+#include <iostream>
+#include <filesystem>
+
+#ifdef _WIN32
+    #include <process.h>
+    #include <windows.h>
+    #include <psapi.h>
+    #define getpid _getpid
+#elif __linux__
+    #include <unistd.h>
+    #include <fstream>
+#elif __APPLE__
+    #include <mach/mach.h>
+#endif
+
+#include "../../../compiler/ast/ast.h"
+#include "../../../compiler/ast/value.h"
+
+void setupVCore(SymbolContainer& env, StringPool& pool);
