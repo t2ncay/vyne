b99f9895e6a9c733f5502a145e135e673c05a275
Tuncay <qtuncay70@gmail.com>
2026-02-25 20:11:32 +0400

    factor(vfs) : added Vyne File System module base for file operations & updated builder systems

---
+++ vyne/modules/common/vfs/vfs.cpp
@@ -0,0 +1,23 @@
+#include "vfs.h"
+
+/**
+ * VFS Native Method Implementations
+ */
+
+namespace VFsNative {
+    
+}
+
+void setupVFs(SymbolContainer& env, StringPool& pool) {
+    const std::string& path = "global.vfs";
+    
+    if (env.find(path) == env.end()) {
+        env[path] = SymbolTable();
+    }
+
+    auto& vfs = env[path];
+
+    // vfs methods
+
+    // vfs properties
+}
