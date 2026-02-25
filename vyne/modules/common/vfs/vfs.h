616cca022b9b175aa6032cbec508571f8db33eb1
Vahid Khalafov <vahid.khalafovv@gmail.com>
2026-02-25 21:07:57 +0400

    updated vfs header file

--- vyne/modules/common/vfs/vfs.h
+++ vyne/modules/common/vfs/vfs.h
@@ -6,6 +6,11 @@
 #include <chrono>
 #include <cstring>
 #include <cmath>
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 
 #include "../../../compiler/ast/ast.h"
 #include "../../../compiler/ast/value.h"
